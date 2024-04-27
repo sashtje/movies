5ba01cf544d839f6afa2644d29895a218c6eaf73
sashtje <sashtje.lefevre@gmail.com>
2024-04-27 10:55:58 +0300

    feat: add react and sass

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+
+import { App } from "./app/App";
+
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("There is no root container in html file");
+}
+
+const root = createRoot(container);
+
+root.render(<App />);
